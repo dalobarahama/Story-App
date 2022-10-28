33982b31b01bec857bfdeb10a590d6dc075c0478
Dalo Barahama <das.barahama@gmail.com>
2022-10-28 10:02:39 +0700

    add story list item

--- app/src/main/java/com/example/storyapp/auth/LoginActivity.kt
+++ app/src/main/java/com/example/storyapp/auth/LoginActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.storyapp.MainActivity
+import com.example.storyapp.view.MainActivity
 import com.example.storyapp.R
 
 class LoginActivity : AppCompatActivity() {
