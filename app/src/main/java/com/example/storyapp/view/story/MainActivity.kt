e0a7b330c65658904af40d76f58307f5f8cb0f65
Dalo Barahama <das.barahama@gmail.com>
2022-10-31 09:58:04 +0700

    add add story page

--- app/src/main/java/com/example/storyapp/view/story/MainActivity.kt
+++ app/src/main/java/com/example/storyapp/view/story/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.storyapp.R
+import com.example.storyapp.view.addstory.AddStoryActivity
 import com.example.storyapp.view.detailstory.DetailStoryActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +15,16 @@ class MainActivity : AppCompatActivity() {
 
         val tempButton = findViewById<Button>(R.id.temp_button)
         tempButton.setOnClickListener { goToDetailStory() }
+
+        val tempButton2 = findViewById<Button>(R.id.temp_button2)
+        tempButton2.setOnClickListener { goToAddStory() }
     }
 
     private fun goToDetailStory() {
         startActivity(Intent(this, DetailStoryActivity::class.java))
+    }
+
+    private fun goToAddStory() {
+        startActivity(Intent(this, AddStoryActivity::class.java))
     }
 }
