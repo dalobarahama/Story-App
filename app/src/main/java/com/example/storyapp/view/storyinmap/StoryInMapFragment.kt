2e437af6fbfb12888f2a4fd82b6d25784dc92e06
Dalo Barahama <das.barahama@gmail.com>
2022-11-15 14:08:06 +0700

    get device location

--- app/src/main/java/com/example/storyapp/view/storyinmap/StoryInMapFragment.kt
+++ app/src/main/java/com/example/storyapp/view/storyinmap/StoryInMapFragment.kt
@@ -1,16 +1,25 @@
 package com.example.storyapp.view.storyinmap
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.storyapp.R
 import com.example.storyapp.data.model.StoryModel
 import com.example.storyapp.viewmodel.StoryViewModel
 import com.example.storyapp.viewmodel.ViewModelFactory
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
@@ -27,6 +36,10 @@ class StoryInMapFragment : Fragment(), OnMapReadyCallback {
         ViewModelFactory()
     }
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var locationPermissionGranted = true
+    private var lastKnownLocation: Location? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -37,6 +50,19 @@ class StoryInMapFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if (ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        }
+
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+
         val mapView = view.findViewById<MapView>(R.id.map)
         mapView.onCreate(savedInstanceState)
         mapView.onResume()
@@ -45,19 +71,36 @@ class StoryInMapFragment : Fragment(), OnMapReadyCallback {
         val sharedPref = view.context.getSharedPreferences("prefs", Context.MODE_PRIVATE)
         val token = sharedPref.getString("token", "123") ?: ""
 
-//        viewModel.getStoryWithLocation(token)
         viewModel.getStoryWithLocation(token).observe(viewLifecycleOwner) {
-            addManyMarker(it)
+//            addManyMarker(it)
         }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
+        if (ActivityCompat.checkSelfPermission(requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        map.isMyLocationEnabled = true
+
         map.uiSettings.isZoomControlsEnabled = true
         map.uiSettings.isIndoorLevelPickerEnabled = true
         map.uiSettings.isCompassEnabled = true
         map.uiSettings.isMapToolbarEnabled = true
+
+        getDeviceLocation()
     }
 
     private fun addManyMarker(listStory: List<StoryModel>) {
@@ -78,5 +121,45 @@ class StoryInMapFragment : Fragment(), OnMapReadyCallback {
         )
     }
 
-
+    @SuppressLint("MissingPermission")
+    fun getDeviceLocation() {
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude, lastKnownLocation!!.longitude),
+                                14.0f
+                            ))
+                        }
+                    }
+                }
+            } else {
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    LatLng(-6.183102834926161, 106.83094375755323), 14.0f)
+                )
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
